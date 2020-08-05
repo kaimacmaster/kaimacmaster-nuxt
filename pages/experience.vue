6df76aaa6cec9c7f5d00645e1fb436cf713e459b
Kai Macmaster <kaimacmaster@me.com>
2020-08-05 17:01:25 +0100

    Add work experience

--- pages/experience.vue
+++ pages/experience.vue
@@ -1,5 +1,67 @@
 <template>
+  <div class="page">
+    <h1>Experience</h1>
 
+    <article>
+      <h2>
+        Omnifi Ltd
+      </h2>
+      <h3>
+        Full Stack Developer
+      </h3>
+      <p>
+        Developing in Vue and Nuxt across a wide range of retail web projects/products, leveraging AWS to manage CI pipelines, developing in sprints.
+      </p>
+    </article>
+
+    <article>
+      <h2>
+        Mobius Media
+      </h2>
+      <h3>
+        Senior Web Developer
+      </h3>
+      <p>
+        Building Laravel and Vue web applications, managing projects, supporting project managers in providing estimated time scales.
+      </p>
+    </article>
+
+    <article>
+      <h2>
+        Netsells Ltd
+      </h2>
+      <h3>
+        Front End Engineer
+      </h3>
+      <p>
+        Developing in Vue and Laravel to implement changes to YourParkingSpace, overseeing all code pull requests, developing for front-end tasks in Jira, planning ‘epic’ tasks, providing estimates and delivering solutions.
+      </p>
+    </article>
+
+    <article>
+      <h2>
+        Beanmedia Ltd
+      </h2>
+      <h3>
+        Creative Services Director
+      </h3>
+      <p>
+        Managing all of the projects that come through the company. Liaising with customers to plan and execute the build of multiple websites, branding and marketing material packs. Frequent use of the Adobe Creative Cloud suite of applications.
+      </p>
+    </article>
+
+    <article>
+      <h2>
+        Netopian Ltd
+      </h2>
+      <h3>
+        Software Developer, Manager and DevOps
+      </h3>
+      <p>
+        Developing in Java (Native and Spring Boot), PHP (Native, Slim and Laravel) and Javascript (Native, jQuery, React and Angular). Planning all work projects in development, training employees, managing holiday requests.
+      </p>
+    </article>
+  </div>
 </template>
 
 <script>
@@ -8,6 +70,9 @@ export default {
 }
 </script>
 
-<style scoped>
-
+<style lang="scss" scoped>
+article {
+  max-width: 32rem;
+  padding: 1rem 0 2rem;
+}
 </style>
