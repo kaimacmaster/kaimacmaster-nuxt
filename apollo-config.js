92b16b726e600b4f797fea98c84170e7724e2299
Kai Macmaster <kaimacmaster@me.com>
2020-09-22 17:17:14 -0400

    Update apollo-config.js to use context env-vars

--- apollo-config.js
+++ apollo-config.js
@@ -1,11 +1,9 @@
 export default (context) => {
-  console.log(context.env.NUXT_ENV_SPACE_ID)
-
   return {
     httpEndpoint: `https://graphql.contentful.com/content/v1/spaces/${context.env.NUXT_ENV_SPACE_ID}`,
     httpLinkOptions: {
       credentials: 'same-origin',
     },
-    getAuth: () => `Bearer phAyBqz84mlZiQNu598NTGh14RSfL65cf6xmSDXJ3rI`,
+    getAuth: () => `Bearer ${context.env.NUXT_ENV_CDA_TOKEN}`,
   }
 }
