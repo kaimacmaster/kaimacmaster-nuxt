1d38eff2165f9e3451aa346c1a7dc8e9f6f67c10
Kai Macmaster <kaimacmaster@me.com>
2020-09-22 17:27:14 -0400

    Add global.scss

--- nuxt.config.js
+++ nuxt.config.js
@@ -14,7 +14,7 @@ export default {
     ],
     link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }],
   },
-  css: [],
+  css: ['~/assets/scss/global.scss'],
   plugins: [],
   components: true,
   buildModules: [],
--- pages/index.vue
+++ pages/index.vue
@@ -8,10 +8,12 @@
     <h1 v-text="name" />
     <section class="headingMd">
       <p>
-        I'm a full-stack web developer specialising in front-end development with over nine years working in industry.
+        I'm a full-stack web developer specialising in front-end development
+        with over nine years working in industry.
       </p>
       <p>
-        Proficient in all stages of development including dev-ops, front-end, back-end, architecture and web design.
+        Proficient in all stages of development including dev-ops, front-end,
+        back-end, architecture and web design.
       </p>
     </section>
     <a href="mailto:kai@macmaster.co.uk">kai@macmaster.co.uk</a>
@@ -21,45 +23,7 @@
 <script>
 export default {
   data: () => ({
-    name: 'Kai Macmaster'
-  })
+    name: 'Kai Macmaster',
+  }),
 }
 </script>
-
-<style lang="scss">
-.page {
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  justify-content: center;
-  padding: 1rem 0.5rem;
-
-  @media only screen and (min-width: 32.5rem) {
-    text-align: center;
-  }
-}
-
-.borderCircle {
-  border-radius: 9999px;
-}
-
-.headerHomeImage {
-  width: 8rem;
-  height: 8rem;
-}
-
-.headingMd {
-  max-width: 32rem;
-  text-align: center;
-  padding: 0 0.25rem;
-
-  @media only screen and (min-width: 32.5rem) {
-    font-size: 1.2rem;
-    line-height: 1.5;
-  }
-
-  p {
-    padding-bottom: 1rem;
-  }
-}
-</style>
